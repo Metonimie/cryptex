0393962f9d25b58f5056452ab4adc1e81f4a736a
Denis Nuțiu <nutiudenis@hotmail.com>
2016-08-20 20:21:32 +0300

    Initial prototype

---
+++ templates/main_window.js
@@ -0,0 +1,58 @@
+"use strict";
+
+console.log("Hello");
+
+function copyText() {
+    console.log("copyText called!");
+    var field = document.querySelector("textarea");
+    field.focus();
+    field.setSelectionRange(0, field.value.length);
+    var copysuccess;
+    try {
+        copysuccess = document.execCommand("copy");
+    } catch(e) {
+        copysuccess = false;
+    }
+    console.log("copyText exit!");
+}
+
+function encryptText() {
+    console.log("encryptText called!");
+
+    var key = document.getElementById("password");
+    var text = document.querySelector("textarea");
+
+    var encrypted = CryptoJS.AES.encrypt(text.value, key.value);
+    text.value = encrypted.toString();
+    copyText();
+
+    console.log("encryptText exit!");
+}
+
+function decryptText() {
+    console.log("decryptText called!");
+
+    var key = document.getElementById("password");
+    var text = document.querySelector("textarea");
+
+    console.log(key.value);
+    var decrypted = CryptoJS.AES.decrypt(text.value, key.value);
+    text.value = decrypted.toString(CryptoJS.enc.Utf8);
+    copyText();
+
+    console.log("decryptText exit!");
+}
+
+(function () {
+    var
+        copy_button       = document.getElementById("copy_button"),
+        decrypt_button    = document.getElementById("decrypt_button"),
+        encrypt_button    = document.getElementById("encrypt_button");
+
+        // Add event listeners
+        copy_button.addEventListener("click", copyText, false);
+        encrypt_button.addEventListener("click", encryptText, false);
+        decrypt_button.addEventListener("click", decryptText, false);
+})();
+
+console.log("Bye!");
